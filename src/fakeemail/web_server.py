801413f817b6c948c31f41e4a230d153040a26b9
Tom Wardill <tom@howrandom.net>
2010-10-05 21:56:22 +0800

    Added classes for data api

--- src/fakeemail/web_server.py
+++ src/fakeemail/web_server.py
@@ -46,6 +46,47 @@ class WebMessageRootDisplay(Resource):
         self.storage.clear_all()
         return self.internal_render('Data Cleared')
     
+class WebDataMessageDisplay(Resource):
+
+    def __init__(self, name, storage):
+        Resource.__init__(self)
+        self.name = name
+        self.storage = storage
+
+    def render_GET(self, request):
+        
+        data = {'email_list' : self.storage.get_for_name(self.name),
+                'address_name' : self.name,
+                }
+        
+        env = Environment(loader = PackageLoader('fakeemail', 'templates'))
+        template = env.get_template('data_message_display.html')
+        
+        return template.render(data).encode('utf-8')
+    
+class WebDataRootDisplay(Resource):
+    
+    def __init__(self, name, storage):
+        Resource.__init__(self)
+        self.storage = storage
+        
+    def internal_render(self, user_message = None):
+        address_list = [ [self.storage.get_count(name), name] for name in self.storage.get_all_names()]
+        data = {'address_list': address_list, 'user_message': user_message}
+        
+        env = Environment(loader = PackageLoader('fakeemail', 'templates'))
+        template = env.get_template('data_home_page.html')
+        
+        return template.render(data).encode('utf-8')
+        
+    def render_GET(self, request, user_message = None):
+        
+        return self.internal_render()
+    
+    def render_POST(self, request):
+        self.storage.clear_all()
+        return self.internal_render('Data Cleared')
+    
 class WebMessageRouter(Resource):
 
     def __init__(self, storage):
