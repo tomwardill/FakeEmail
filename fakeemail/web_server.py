ed2f382335fa547b38933bfa6b7442956d8338e6
Low Kian Seong <kianseong@gmail.com>
2014-07-13 19:21:59 +0800

    Adding description to setup.py and pep8 cleanup

--- fakeemail/web_server.py
+++ fakeemail/web_server.py
@@ -12,6 +12,7 @@ try:
 except:
         import simplejson as json
 
+
 class WebMessageDisplay(Resource):
 
     def __init__(self, name, storage):
@@ -40,39 +41,40 @@ class WebMessageDisplay(Resource):
                     parts['payload'] = payload
                 decoded_email_list.append(parts)
 
-        data = {'email_list' : decoded_email_list,
-                'address_name' : self.name,
-                }
-        
-        env = Environment(loader = PackageLoader('fakeemail', 'templates'))
+        data = {'email_list': decoded_email_list,
+                'address_name': self.name, }
+
+        env = Environment(loader=PackageLoader('fakeemail', 'templates'))
         template = env.get_template('message_display.html')
-        
+
         return template.render(data).encode('UTF-8')
-        
+
+
 class WebMessageRootDisplay(Resource):
-    
+
     def __init__(self, name, storage):
         Resource.__init__(self)
         self.storage = storage
-        
-    def internal_render(self, user_message = None):
-        address_list = [ [self.storage.get_count(name), name] for name in self.storage.get_all_names()]
+
+    def internal_render(self, user_message=None):
+        address_list = [[self.storage.get_count(name), name]
+                        for name in self.storage.get_all_names()]
         data = {'address_list': address_list, 'user_message': user_message}
-        
-        env = Environment(loader = PackageLoader('fakeemail', 'templates'))
+
+        env = Environment(loader=PackageLoader('fakeemail', 'templates'))
         template = env.get_template('home_page.html')
-        
+
         return template.render(data).encode('utf-8')
-        
-    def render_GET(self, request, user_message = None):
-        
+
+    def render_GET(self, request, user_message=None):
+
         return self.internal_render()
-        
-    
+
     def render_POST(self, request):
         self.storage.clear_all()
         return self.internal_render('Data Cleared')
-    
+
+
 class WebDataMessageDisplay(Resource):
 
     def __init__(self, name, storage):
@@ -81,47 +83,48 @@ class WebDataMessageDisplay(Resource):
         self.storage = storage
 
     def render_GET(self, request):
-        
-        data = {'email_list' : self.storage.get_for_name(self.name),
-                'address_name' : self.name,
-                }
-        
-        env = Environment(loader = PackageLoader('fakeemail', 'templates'))
+
+        data = {'email_list': self.storage.get_for_name(self.name),
+                'address_name': self.name, }
+
+        env = Environment(loader=PackageLoader('fakeemail', 'templates'))
         template = env.get_template('data_message_display.html')
-        
+
         data_dump = {'data': json.dumps(data)}
-        
+
         return template.render(data_dump).encode('utf-8')
-    
-    
+
+
 class WebDataRootDisplay(Resource):
-    
+
     def __init__(self, name, storage):
         Resource.__init__(self)
         self.storage = storage
-        
-    def internal_render(self, user_message = ''):
-        address_list = [ [self.storage.get_count(name), name] for name in self.storage.get_all_names()]
+
+    def internal_render(self, user_message=''):
+        address_list = [[self.storage.get_count(name), name]
+                        for name in self.storage.get_all_names()]
         data = {'address_list': address_list, 'user_message': user_message}
-        
-        env = Environment(loader = PackageLoader('fakeemail', 'templates'))
+
+        env = Environment(loader=PackageLoader('fakeemail', 'templates'))
         template = env.get_template('data_home_page.html')
-        
-        data_dump = {'data':json.dumps(data)}
-        
+
+        data_dump = {'data': json.dumps(data)}
+
         return template.render(data_dump).encode('utf-8')
-        
-    def render_GET(self, request, user_message = None):
-        
+
+    def render_GET(self, request, user_message=None):
+
         return self.internal_render()
-    
+
     def render_POST(self, request):
         self.storage.clear_all()
         return self.internal_render('Data Cleared')
-    
+
     def getChild(self, name, request):
         return WebDataMessageDisplay(name, self.storage)
-    
+
+
 class WebMessageRouter(Resource):
 
     def __init__(self, storage):
@@ -132,11 +135,12 @@ class WebMessageRouter(Resource):
         if name:
             if name == 'data':
                 return WebDataRootDisplay('', self.storage)
-            
+
             return WebMessageDisplay(name, self.storage)
         else:
             return WebMessageRootDisplay(name, self.storage)
-        
+
+
 def makeWebService(web_interface, web_port, storage):
     root = WebMessageRouter(storage)
     siteFactory = Site(root)
--- setup.py
+++ setup.py
@@ -1,23 +1,24 @@
 from setuptools import setup
 
 setup(
-    name = 'fakeemail',
-    version = '0.8',
-    author = 'Tom Wardill',
-    author_email = 'tom@howrandom.net',
-    url = 'https://github.com/tomwardill/FakeEmail',
-    zip_safe = True,
-    packages = ['fakeemail', 'twisted.plugins'],
-    install_requires = [
+    name='fakeemail',
+    version='0.8',
+    author='Tom Wardill',
+    author_email='tom@howrandom.net',
+    description='A fake Email Server with a Web Front End',
+    url='https://github.com/tomwardill/FakeEmail',
+    zip_safe=True,
+    packages=['fakeemail', 'twisted.plugins'],
+    install_requires=[
         'Twisted', 'jinja2',
     ],
     package_data={
         'twisted': ['plugins/fakeemail.py'],
         'fakeemail': ['templates/*']
         },
-    entry_points = {
-        'console_scripts' : [
-            'fakeemail = fakeemail.server:start'
+    entry_points={
+        'console_scripts': [
+            'fakeemail=fakeemail.server:start'
         ]
     }
 )
