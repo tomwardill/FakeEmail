863c40a94da306587d8733433ac3bf64a975b450
Tom Wardill <tom@howrandom.net>
2015-09-15 14:08:02 +0100

    Update version number for new pypi release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='fakeemail',
-    version='0.8.1',
+    version='0.9.0',
     author='Tom Wardill',
     author_email='tom@howrandom.net',
     description='A fake Email Server with a Web Front End',
